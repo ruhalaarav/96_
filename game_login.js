1b89da259d9fc47792efb14ba2dd1aad48d4359e
ruhalaarav <112325488+ruhalaarav@users.noreply.github.com>
2022-12-25 16:30:44 +0530

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,17 @@
+
+function addUser() {
+  player1_name = document.getElementById("player1_name_input").value;
+  player2_name = document.getElementById("player2_name_input").value;
+
+    //Set "player1_name" using localStorage.setItem() function
+    function player1_name(){
+    localStorage.setItem()
+    }
+	  //Set "player2_name" using localStorage.setItem() function
+    function player1_name(){
+    localStorage.setItem()
+    }
+
+    window.location = "game_page.html";
+}
+
